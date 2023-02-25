197488364e8c5bc0ad4387e348577d5a83739dbb
paulius <paulius@2simple.com>
2023-02-25 21:25:03 +0000

    create footer container

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 import './FooterOverlay.css';
 
 const FooterOverlay = () => (
-  <div>
-    FooterOverlay
-  </div>
+    <div className="app__footerOverlay">
+        <div className="app__footerOverlay-black"></div>
+        <div className="app__footerOverlay-img app__bg"></div>
+    </div>
 );
 
 export default FooterOverlay;
